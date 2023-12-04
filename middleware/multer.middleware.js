9165d1611fcd4fb891a05c96df1ae66fad03ef9c
tojaarek <119487863+tojaarek@users.noreply.github.com>
2023-12-04 11:27:06 +0100

    tmp to uploads

--- middleware/multer.middleware.js
+++ middleware/multer.middleware.js
@@ -2,7 +2,7 @@ const multer = require('multer');
 const path = require('path');
 
 const upload = multer({
-  dest: path.join(__dirname, '..', 'tmp'),
+  dest: path.join(__dirname, '..', 'uploads'),
   limits: {
     fileSize: 3145728,
   },
