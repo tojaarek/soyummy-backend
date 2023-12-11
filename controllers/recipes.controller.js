3cd2eba5f65f451d4641f53e2c45d6d4605bf37a
tojaarek <119487863+tojaarek@users.noreply.github.com>
2023-12-11 15:42:36 +0100

    bugfix

--- controllers/recipes.controller.js
+++ controllers/recipes.controller.js
@@ -218,26 +218,13 @@ const getRecipeByIdHandler = async (req, res, next) => {
   try {
     const recipeId = req.params.recipeId;
     const recipe = await getRecipeById(recipeId);
-
     if (recipe === null) {
-      return res.status(404).json({
+      res.status(404).json({
         status: 'fail',
         code: 404,
         message: 'Not found',
       });
     }
-
-    if (recipe.owner) {
-      const userId = req.user.id;
-      if (recipe.owner !== userId) {
-        return res.status(403).json({
-          status: 'fail',
-          code: 403,
-          message: 'Forbidden',
-        });
-      }
-    }
-
     res.status(200).json({
       status: 'success',
       code: 200,
